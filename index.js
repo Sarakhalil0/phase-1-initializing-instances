c67dbc063db6164eaea6dc2b755ceeacbde9adab
sara khalil <sarakhalil3377@gmail.com>
2022-09-21 10:51:15 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here
+class Breakfast{
+    constructor(food ,drink){
+        this.food=food
+        this.drink=drink
+}
+}
+
+const breakfast=new Breakfast('pitzza' ,'pepsi')
+Breakfast.salad;
+
+//
+class Lunch{
+    constructor(salad ,soup){
+        this.salad=salad
+        this.soup=soup
+    }
+}
+const lunch=new Lunch('ceser salad' ,'chiken soup')
+Lunch.salad;
+
+//
+class Dinner {
+    #dissert;
+    constructor(salad, soup, entree, dessert){
+        this.salad=salad;
+        this.soup=soup;
+        this.entree=entree;
+        this.#dissert=dissert;
+    }
+}
+
+const dinner=new Dinner('ceser salad' ,'chiken soup')
+Dinner.salad;
